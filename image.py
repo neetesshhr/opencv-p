21007f5f54997495543bb671fa1a1c32fd6f48ab
Nitesh Rijal <rijalboy.94@gmail.com>
2020-02-29 22:54:09 +0545

    mouse_practice

--- image.py
+++ image.py
@@ -2,15 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('nitesh.jpg',0)
+img = cv2.imread('shiva.png')
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OM NAMAH SHIVAY',(10,500), font, 4,(255,255,255),6,cv2.LINE_AA)
+
+
+
 # cv2.imwrite('shiva.png',img)
+
 cv2.namedWindow('image',cv2.WINDOW_NORMAL)
 
-# cv2.imshow('image',img)
+cv2.imshow('image',img)
 
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-plt.show()
+
+# plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+# plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
+# plt.show()
 
 
 key = cv2.waitKey(0)
